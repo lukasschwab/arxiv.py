dbe7a4b29bb8e517b021fe193f17f6d856e3b4be
Lukas Schwab <lukas.schwab@gmail.com>
2015-11-25 11:26:18 -0800

    add utils for PyPI distribution

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+	name="arxiv",
+	version="0.0.1",
+	packages=["arxiv"],
+
+	# dependencies
+	install_requires=[
+		'feedparser',
+		'urllib'
+	],
+
+	# metadata for upload to PyPI
+	authors="Lukas Schwab",
+	author_email="lukas.schwab@gmail.com",
+	description="Python wrapper for the arXiv API: http://arxiv.org/help/api/",
+	license="MIT",
+	keywords="arxiv api wrapper academic journals papers",
+	url="https://github.com/lukasschwab/arxiv.py",
+	download_url="https://github.com/lukasschwab/arxiv.py/tarball/0.0.1",
+)
