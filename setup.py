e8d4cb0477dc55017f1bc289c8a2848e01b3055e
Lukas Schwab <lukas.schwab@gmail.com>
2015-11-25 16:57:41 -0800

    iterate to 0.0.2 because i messed up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="arxiv",
-	version="0.0.1",
+	version="0.0.2",
 	packages=["arxiv"],
 
 	# dependencies
@@ -15,5 +15,5 @@ setup(
 	license="MIT",
 	keywords="arxiv api wrapper academic journals papers",
 	url="https://github.com/lukasschwab/arxiv.py",
-	download_url="https://github.com/lukasschwab/arxiv.py/tarball/0.0.1",
+	download_url="https://github.com/lukasschwab/arxiv.py/tarball/0.0.2",
 )
