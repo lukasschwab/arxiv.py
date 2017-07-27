b97581a1494de8b9a7b1313ba29cc0e11989a701
Lukas Schwab <lukas.schwab@gmail.com>
2017-07-26 18:11:45 -0700

    Version++ because I messed up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="arxiv",
-	version="0.1.1",
+	version="0.2.1",
 	packages=["arxiv"],
 
 	# dependencies
@@ -18,5 +18,5 @@ setup(
 	license="MIT",
 	keywords="arxiv api wrapper academic journals papers",
 	url="https://github.com/lukasschwab/arxiv.py",
-	download_url="https://github.com/lukasschwab/arxiv.py/tarball/0.1.1",
+	download_url="https://github.com/lukasschwab/arxiv.py/tarball/0.2.1",
 )
