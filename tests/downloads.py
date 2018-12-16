b959fc796858a6c34e15bd33db1caab87a95e6f9
Lukas Schwab <lukas.schwab@gmail.com>
2018-12-16 15:09:03 -0800

    Add user-specified obj tests to downloads.py

--- tests/downloads.py
+++ tests/downloads.py
@@ -1,21 +1,20 @@
 import arxiv
 
-paper = arxiv.query(id_list=["1707.08567"])[0]
+paper1 = arxiv.query(id_list=["1707.08567"])[0]
+paper2 = {"pdf_url": "http://arxiv.org/pdf/1707.08567v1",
+          "title": "The Paper Title"}
 
-# Default behavior
-print arxiv.download(paper)
-
-# Specify subdirectory
-print arxiv.download(paper, dirpath="downloads")
-print arxiv.download(paper, dirpath="downloads/")
-
-# Returns the object id.
-def custom_slugify(obj):
-    return obj.get('id').split('/')[-1]
-
-# Specify slugify without subdir
-print arxiv.download(paper, slugify=custom_slugify)
-
-# Specify slugify with subdir
-print arxiv.download(paper, slugify=custom_slugify, dirpath="downloads")
-print arxiv.download(paper, slugify=custom_slugify, dirpath="downloads/")
+for paper in [paper1, paper2]:
+    # Default behavior
+    print arxiv.download(paper)
+    # Specify subdirectory
+    print arxiv.download(paper, dirpath="downloads")
+    print arxiv.download(paper, dirpath="downloads/")
+    # Returns the object id.
+    def custom_slugify(obj):
+        return obj.get('pdf_url').split('/')[-1]
+    # Specify slugify without subdir
+    print arxiv.download(paper, slugify=custom_slugify)
+    # Specify slugify with subdir
+    print arxiv.download(paper, slugify=custom_slugify, dirpath="downloads")
+    print arxiv.download(paper, slugify=custom_slugify, dirpath="downloads/")
