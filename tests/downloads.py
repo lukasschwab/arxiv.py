47f0e1a9be13c03d40a563c1953a96eb9f1efb6a
Lukas Schwab <lukas.schwab@gmail.com>
2018-12-16 15:37:17 -0800

    Make downloads test python version-agnostic

--- tests/downloads.py
+++ tests/downloads.py
@@ -1,4 +1,9 @@
+from __future__ import print_function
 import arxiv
+
+# Returns the object id.
+def custom_slugify(obj):
+    return obj.get('pdf_url').split('/')[-1]
 
 paper1 = arxiv.query(id_list=["1707.08567"])[0]
 paper2 = {"pdf_url": "http://arxiv.org/pdf/1707.08567v1",
@@ -6,15 +11,12 @@ paper2 = {"pdf_url": "http://arxiv.org/pdf/1707.08567v1",
 
 for paper in [paper1, paper2]:
     # Default behavior
-    print arxiv.download(paper)
+    print(arxiv.download(paper))
     # Specify subdirectory
-    print arxiv.download(paper, dirpath="downloads")
-    print arxiv.download(paper, dirpath="downloads/")
-    # Returns the object id.
-    def custom_slugify(obj):
-        return obj.get('pdf_url').split('/')[-1]
+    print(arxiv.download(paper, dirpath="downloads"))
+    print(arxiv.download(paper, dirpath="downloads/"))
     # Specify slugify without subdir
-    print arxiv.download(paper, slugify=custom_slugify)
+    print(arxiv.download(paper, slugify=custom_slugify))
     # Specify slugify with subdir
-    print arxiv.download(paper, slugify=custom_slugify, dirpath="downloads")
-    print arxiv.download(paper, slugify=custom_slugify, dirpath="downloads/")
+    print(arxiv.download(paper, slugify=custom_slugify, dirpath="downloads"))
+    print(arxiv.download(paper, slugify=custom_slugify, dirpath="downloads/"))
