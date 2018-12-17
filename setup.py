e2f4ee4d2357331825c93dfe6b2f7110044637d2
Lukas Schwab <lukas.schwab@gmail.com>
2018-12-16 16:11:38 -0800

    Remove download URL from setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
 	license="MIT",
 	keywords="arxiv api wrapper academic journals papers",
 	url="https://github.com/lukasschwab/arxiv.py",
-	download_url="https://github.com/lukasschwab/arxiv.py/tarball/"+version,
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
