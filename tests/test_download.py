3cc4aeeb0921b438666bc4b2a567b2c1b60a38d6
Tobias Windisch <tobias.windisch@posteo.de>
2018-12-30 13:22:35 +0100

    refactor test

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,22 +1,30 @@
 from __future__ import print_function
 import arxiv
+import tempfile
+import unittest
 
-# Returns the object id.
-def custom_slugify(obj):
-    return obj.get('pdf_url').split('/')[-1]
 
-paper1 = arxiv.query(id_list=["1707.08567"])[0]
-paper2 = {"pdf_url": "http://arxiv.org/pdf/1707.08567v1",
-          "title": "The Paper Title"}
+class TestDownload(unittest.TestCase):
 
-for paper in [paper1, paper2]:
-    # Default behavior
-    print(arxiv.download(paper))
-    # Specify subdirectory
-    print(arxiv.download(paper, dirpath="downloads"))
-    print(arxiv.download(paper, dirpath="downloads/"))
-    # Specify slugify without subdir
-    print(arxiv.download(paper, slugify=custom_slugify))
-    # Specify slugify with subdir
-    print(arxiv.download(paper, slugify=custom_slugify, dirpath="downloads"))
-    print(arxiv.download(paper, slugify=custom_slugify, dirpath="downloads/"))
+    def test_custom_slugify(self):
+
+        # Returns the object id.
+        def custom_slugify(obj):
+            return obj.get('pdf_url').split('/')[-1]
+
+        paper1 = arxiv.query(id_list=["1707.08567"])[0]
+        paper2 = {"pdf_url": "http://arxiv.org/pdf/1707.08567v1",
+                  "title": "The Paper Title"}
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            for paper in [paper1, paper2]:
+                # Default behavior
+                print(arxiv.download(paper))
+                # Specify subdirectory
+                print(arxiv.download(paper, dirpath=temp_dir))
+                print(arxiv.download(paper, dirpath=temp_dir))
+                # Specify slugify without subdir
+                print(arxiv.download(paper, slugify=custom_slugify))
+                # Specify slugify with subdir
+                print(arxiv.download(paper, slugify=custom_slugify, dirpath=temp_dir))
+                print(arxiv.download(paper, slugify=custom_slugify, dirpath=temp_dir))
