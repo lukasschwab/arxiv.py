e3979d099f93077221a62b0e53ed6195b976ee0f
Tobias Windisch <tobias.windisch@posteo.de>
2019-05-14 23:04:28 +0200

    refactor and speed up download tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,30 +1,61 @@
-from __future__ import print_function
 import arxiv
+import os
 import tempfile
 import unittest
 
 
+# Returns the object id.
+def custom_slugify(obj):
+    return obj.get('pdf_url').split('/')[-1]
+
+
 class TestDownload(unittest.TestCase):
 
-    def test_custom_slugify(self):
+    @classmethod
+    def setUpClass(self):
 
-        # Returns the object id.
-        def custom_slugify(obj):
-            return obj.get('pdf_url').split('/')[-1]
+        self.paper_query = arxiv.query(id_list=["1605.08386"])[0]
+        self.paper_dict = {
+            "pdf_url": "http://arxiv.org/pdf/1605.08386v1",
+            "title": "The Paper Title"}
 
-        paper1 = arxiv.query(id_list=["1707.08567"])[0]
-        paper2 = {"pdf_url": "http://arxiv.org/pdf/1707.08567v1",
-                  "title": "The Paper Title"}
-
+    def test_download_with_custom_slugify_from_query(self):
         with tempfile.TemporaryDirectory() as temp_dir:
-            for paper in [paper1, paper2]:
-                # Default behavior
-                print(arxiv.download(paper))
-                # Specify subdirectory
-                print(arxiv.download(paper, dirpath=temp_dir))
-                print(arxiv.download(paper, dirpath=temp_dir))
-                # Specify slugify without subdir
-                print(arxiv.download(paper, slugify=custom_slugify))
-                # Specify slugify with subdir
-                print(arxiv.download(paper, slugify=custom_slugify, dirpath=temp_dir))
-                print(arxiv.download(paper, slugify=custom_slugify, dirpath=temp_dir))
+            arxiv.download(self.paper_query, slugify=custom_slugify, dirpath=temp_dir)
+            self.assertTrue(
+                    os.path.exists(
+                        os.path.join(temp_dir, '1605.08386v1.pdf')
+                    )
+            )
+
+    def test_download_with_custom_slugify_from_dict(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            arxiv.download(self.paper_dict, slugify=custom_slugify, dirpath=temp_dir)
+            self.assertTrue(
+                    os.path.exists(
+                        os.path.join(temp_dir, '1605.08386v1.pdf')
+                    )
+            )
+
+    def test_download_from_dict(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            arxiv.download(self.paper_dict, dirpath=temp_dir)
+            self.assertTrue(
+                    os.path.exists(
+                        os.path.join(
+                            temp_dir,
+                            '1605.08386v1.The_Paper_Title.pdf')
+                    )
+            )
+
+    def test_download_from_query(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            arxiv.download(self.paper_query, dirpath=temp_dir)
+
+            self.assertTrue(
+                    os.path.exists(
+                        os.path.join(
+                            temp_dir,
+                            '1605.08386v1.Heat_bath_random_walks_with_Markov_bases.pdf')
+                    )
+            )
