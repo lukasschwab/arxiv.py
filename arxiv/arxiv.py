9ba43663e276e466795c1e975a9d13bc8a34ecf9
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-06-09 08:29:25 -0700

    Update arxiv.py
    added variable `start`

--- arxiv/arxiv.py
+++ arxiv/arxiv.py
@@ -25,6 +25,7 @@ class Search(object):
         id_list (list): List of arXiv IDs to be downloaded
         max_results (int): The maximum number of abstracts that should be downloaded. Defaults to
             infinity, i.e., no limit at all
+        start (int): Where to start downloading within results returned. 
         sort_by (string): The arXiv field by which the result should be sorted
         sort_order (string): The sorting order, i.e. "ascending", "descending" or None.
         max_chunk_results (int): Internally, a arXiv search query is split up into smaller
@@ -51,7 +52,7 @@ class Search(object):
         'tags',
         'id']
 
-    def __init__(self, query=None, id_list=None, max_results=None, sort_by=None,
+    def __init__(self, query=None, id_list=None, max_results=None, start = 0, sort_by=None,
                  sort_order=None, max_chunk_results=None, time_sleep=3, prune=True):
 
         self.query = query
@@ -62,6 +63,7 @@ class Search(object):
         self.time_sleep = time_sleep
         self.prune = prune
         self.max_results = max_results
+        self.start = start
 
         if not self.max_results:
             logger.info('No maximal number of results given by the user. Download all')
@@ -140,8 +142,8 @@ class Search(object):
 
     def _get_next(self):
 
-        n_left = self.max_results
-        start = 0
+        n_left = self.max_results - self.start
+        start = self.start
 
         while n_left > 0:
 
@@ -204,7 +206,7 @@ class Search(object):
             return results
 
 
-def query(search_query="", id_list=[], prune=True, max_results=None, sort_by="relevance",
+def query(search_query="", id_list=[], prune=True, max_results=None, start = 0, sort_by="relevance",
           sort_order="descending", max_chunk_results=1000, iterative=False):
     """
     See :py:class:`arxiv.Search` for a description of the parameters.
@@ -217,6 +219,7 @@ def query(search_query="", id_list=[], prune=True, max_results=None, sort_by="re
         sort_order=sort_order,
         prune=prune,
         max_results=max_results,
+        start = start,
         max_chunk_results=max_chunk_results)
 
     return search.download(iterative=iterative)
