48aa7b8fbbc8409a0aba2dddcdcf4e842fbe0a51
Lukas Schwab <lukas.schwab@gmail.com>
2019-06-15 15:40:57 -0700

    0.5.1: use README as pypi long description

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
-version = "0.5.0"
+version = "0.5.1"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="arxiv",
@@ -20,6 +23,8 @@ setup(
     author="Lukas Schwab",
     author_email="lukas.schwab@gmail.com",
     description="Python wrapper for the arXiv API: http://arxiv.org/help/api/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     keywords="arxiv api wrapper academic journals papers",
     url="https://github.com/lukasschwab/arxiv.py",
