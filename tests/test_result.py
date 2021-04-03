d8a5bc5f706eaaa03436342dee5a2f04b32cf63a
Lukas Schwab <lukas.schwab@gmail.com>
2021-04-03 12:28:44 -0700

    Add tests for result class

---
+++ tests/test_result.py
@@ -0,0 +1,51 @@
+import arxiv, unittest, re
+
+
+class TestAPI(unittest.TestCase):
+
+    def assert_nonempty(self, s):
+        self.assertIsNotNone(s)
+        self.assertNotEqual(s, '')
+
+    def assert_valid_author(self, a: arxiv.Result.Author):
+        self.assert_nonempty(a.name)
+    
+    def assert_valid_link(self, l: arxiv.Result.Link):
+        self.assert_nonempty(l.href)
+
+    def assert_valid_result(self, result: arxiv.Result):
+        self.assert_nonempty(result.entry_id)
+        self.assertIsNotNone(result.updated)
+        self.assertIsNotNone(result.published)
+        self.assert_nonempty(result.title)
+        self.assertNotEqual(len(result.authors), 0)
+        for author in result.authors:
+            self.assert_valid_author(author)
+        self.assert_nonempty(result.summary)
+        self.assert_nonempty(result.primary_category)
+        self.assertNotEqual(len(result.categories), 0)
+        for category in result.categories:
+            self.assert_nonempty(category)
+        for link in result.links:
+            self.assert_valid_link(link)
+
+    def test_result_shape(self):
+        max_results = 100
+        results = [r for r in arxiv.Search("testing", max_results=max_results).get()]
+        self.assertEqual(len(results), max_results)
+        for result in results:
+            self.assert_valid_result(result)
+    
+    def test_from_feed_entry(self):
+        feed = arxiv.Client()._parse_feed("http://export.arxiv.org/api/query?search_query=testing")
+        feed_entry = feed.entries[0]
+        result = arxiv.Result._from_feed_entry(feed_entry)
+        self.assert_valid_result(result)
+
+    def test_get_short_id(self):
+        result_id = "1707.08567"
+        result = next(arxiv.Search(id_list=[result_id]).get())
+        got = result.get_short_id()
+        self.assertTrue(got.startswith(result_id))
+        # Should be of form `1707.08567v1`.
+        self.assertTrue(re.match(r'^{}v\d+$'.format(result_id), got))
