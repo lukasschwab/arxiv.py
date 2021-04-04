c048ed29181db25ba64d421261bb0eae9975254e
Lukas Schwab <lukas.schwab@gmail.com>
2021-04-03 21:12:18 -0700

    Tidy dev dependencies: add requirements.txt

--- setup.py
+++ setup.py
@@ -1,36 +1,33 @@
 from setuptools import setup
 
-version = "1.0.0"
+version = '1.0.0'
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name="arxiv",
+    name='arxiv',
     version=version,
-    packages=["arxiv"],
+    packages=['arxiv'],
     # dependencies
-    install_requires=[
-        'feedparser',
-        'requests',
-    ],
+    install_requires=['feedparser'],
     tests_require=[
-        "pytest",
-        "pdoc",
-        "flake8"
+        'pytest',
+        'pdoc',
+        'flake8'
     ],
     # metadata for upload to PyPI
-    author="Lukas Schwab",
-    author_email="lukas.schwab@gmail.com",
-    description="Python wrapper for the arXiv API: http://arxiv.org/help/api/",
+    author='Lukas Schwab',
+    author_email='lukas.schwab@gmail.com',
+    description='Python wrapper for the arXiv API: http://arxiv.org/help/api/',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    keywords="arxiv api wrapper academic journals papers",
-    url="https://github.com/lukasschwab/arxiv.py",
+    long_description_content_type='text/markdown',
+    license='MIT',
+    keywords='arxiv api wrapper academic journals papers',
+    url='https://github.com/lukasschwab/arxiv.py',
     classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
