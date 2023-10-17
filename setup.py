0f6a37fc19e3a4ef0f904e084f54a6d2b6c7d3be
Lukas Schwab <lukas.schwab@gmail.com>
2023-10-16 21:11:09 -0700

    Pin feedparser 6.0.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=["arxiv"],
     # dependencies
     python_requires=">=3.7",
-    install_requires=["feedparser>=6.0.6", "requests==2.31.0"],
+    install_requires=["feedparser==6.0.10", "requests==2.31.0"],
     tests_require=["pytest", "pdoc", "ruff"],
     # metadata for upload to PyPI
     author="Lukas Schwab",
