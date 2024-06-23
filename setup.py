7edcd0e8c880f14f2abeb4cbe36686b7f01c05be
kgleba <kgleba@yandex.ru>
2024-06-23 21:58:05 +0300

    Loosen requirements in `setup.py` (#165)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=["arxiv"],
     # dependencies
     python_requires=">=3.7",
-    install_requires=["feedparser==6.0.10", "requests==2.32.0"],
+    install_requires=["feedparser~=6.0.10", "requests~=2.32.0"],
     tests_require=["pytest", "pdoc", "ruff"],
     # metadata for upload to PyPI
     author="Lukas Schwab",
